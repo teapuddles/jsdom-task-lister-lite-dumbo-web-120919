76b6e3bbcb5604b36f3c7a920d21f43a217d8e31
teapuddles <kingsley.gleeson@gmail.com>
2020-01-31 13:45:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+//grab all the necessary DOM elements
+
+const taskForm = document.querySelector("#create-task-form")
+const taskContent = document.querySelector("#new-task-description")
+//form and relevant input fields
+
+const taskList = document.querySelector("#tasks")
+//ul where new tasks will live on the DOM
+
+const newTask = document.addEventListener("submit", createNewTask);
+//attach event listeners
+
 });
+
+const createNewTask = e => {
+  e.preventDefault();
+  const taskContent = document.querySelector("#new-task-description");
+  const newTask = document.createElement('li')
+    newTask.innerText = taskContent.value;
+
+    appendNewTask(newTask)
+}
+
+const appendNewTask = task => {
+  document.querySelector("#tasks").appendChild(task);
+}
